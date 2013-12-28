441b84595bc938f190347683e7d8e87d6e6403e2
Josh Milstead <jmilst20@gmail.com>
2013-12-28 17:38:52 -0500

    added level buttons

--- dare28/src/com/intelligentbeans/dare/Dare.java
+++ dare28/src/com/intelligentbeans/dare/Dare.java
@@ -15,4 +15,3 @@ public class Dare extends  Game {
 			
 		}
 	}
-
--- dare28/src/com/intelligentbeans/dare/LevelMenu.java
+++ dare28/src/com/intelligentbeans/dare/LevelMenu.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.intelligentbeans.boilerplate.SpriteImage;
 
@@ -31,8 +32,12 @@ public class LevelMenu extends SpriteImage {
     protected SpriteImage progressIndicator;
     protected float maxWidth = 502;
     protected Preferences prefs;
+    protected int numLevels = 9;
+    protected Array<SpriteImage> buttons;
 	public LevelMenu(Vector2 position, Stage stage){
 		super(position, "levelMenu");
+		
+		buttons = new Array<SpriteImage>(numLevels);
 		/*
 		progress = new SpriteImage(position, "levelprogress");
 		progressIndicator =  new SpriteImage(position, "levelindicator");
@@ -46,7 +51,10 @@ public class LevelMenu extends SpriteImage {
 		progressIndicator.setX(progress.getX() + progress.getWidth() - (progressIndicator.getWidth()/2));
 		progressIndicator.setY(this.getY());
 		*/
-		stage.addActor(this);	
+		stage.addActor(this);
+		
+		
+	
 		/*
 		stage.addActor(progress);
 		stage.addActor(progressIndicator);
@@ -54,11 +62,88 @@ public class LevelMenu extends SpriteImage {
 	
 		prefs = Gdx.app.getPreferences("my-preferences");
 		
-		
+		int highestLevel = prefs.getInteger("HigestLevelComplete");
 		int currentLevel = prefs.getInteger("CurrentLevel");
 		Gdx.app.log("currentLevel ", currentLevel + "");
 		
-	 		
+		
+		for( int i = 0; i< numLevels; i ++){
+			SpriteImage button;
+			if( i + 1 < highestLevel || i == 0){
+				button =  new SpriteImage(position, "levelOptionActive");
+			}else{
+				button =  new SpriteImage(position, "levelOptionInactive");
+			}
+			buttons.add(button);
+			
+			stage.addActor(button);
+		}
+		positionButtons();
+		
+	}
+	
+	public void positionButtons(){
+		
+		int xCount = 0;
+		int yCount = 0;
+		for( int i = 0; i< numLevels; i ++){
+			SpriteImage button = buttons.get(i);
+			button.setX((this.getX() + 220) + (xCount * (button.getWidth() + 10)));
+			button.setY((this.getY() + this.getHeight()- 270) - (yCount * (button.getHeight() + 10)));
+			
+			xCount ++;
+			if(xCount > 2){
+				xCount = 0;
+				yCount ++;
+			}
+			
+		}
+	}
+	
+	
+	public void hideButtons(){
+			
+	
+			for( int i = 0; i< numLevels; i ++){
+				SpriteImage button = buttons.get(i);
+				button.setVisible(false);
+				
+			}
+	}
+	
+	public void showButtons(){
+		this.toFront();
+	
+		for( int i = 0; i< numLevels; i ++){
+			SpriteImage button = buttons.get(i);
+			button.setVisible(true);
+			button.toFront();
+			
+		}
+	}
+	
+	
+	
+	@Override
+	public  void setVisible(boolean val){
+		super.setVisible(val);
+		if(val){
+			showButtons();
+		}else{
+			hideButtons();
+		}
+		
+	}
+	@Override
+	public  void setX(float val){
+		super.setX(val);
+		positionButtons();
+	}
+	
+	@Override
+	public  void setY(float val){
+		super.setY(val);
+		positionButtons();
 	}
 	
 	
